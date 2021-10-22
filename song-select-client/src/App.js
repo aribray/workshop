c500376ad7b2748619cd24d1f1fd609b27882b43
MiraMarshall <marshallscm@gmail.com>
2021-10-21 21:49:19 -0500

    auth setup

---
+++ song-select-client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login';
+import Home from './Home';
+
+const code = new URLSearchParams(window.location.search).get('code');
+
+function App() {
+  return code ? <Home code={code}/> : <Login/>
+}
+
+export default App;
